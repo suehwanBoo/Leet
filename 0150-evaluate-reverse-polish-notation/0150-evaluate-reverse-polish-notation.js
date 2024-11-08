f18fe8f398345bcc411fcc25acd53b4e3bb0f644
suehwanBoo <130543156+suehwanBoo@users.noreply.github.com>
2024-11-08 17:27:06 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(tokens) {
+    const saveNum = [];
+    for(let token of tokens) {
+        if(!isNaN(+token)) {
+            saveNum.push(+token);
+        } else {
+            const secondNum = saveNum.pop();
+            const firstNum = saveNum.pop();
+            if(token === "+") saveNum.push(firstNum + secondNum);
+            else if(token === "-") saveNum.push(firstNum - secondNum);
+            else if(token === "*") saveNum.push(firstNum * secondNum);
+            else saveNum.push(Math.floor(firstNum / secondNum));
+        }
+        console.log(saveNum)
+    }
+    return saveNum[0];
+};
