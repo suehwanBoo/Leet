d1463898582ed01f00f6ee8aaecbe3fa69ad8f06
suehwanBoo <130543156+suehwanBoo@users.noreply.github.com>
2024-11-23 16:15:25 +0900

    Time: 2 ms (78.46%), Space: 56.4 MB (98.73%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -7,13 +7,10 @@ var maxArea = function(height) {
     let right = height.length - 1;
     let result = 0;
     while(left < right) {
-        let max = 0;
-        for(let i = right; i > left && max < height[left] * (i - left); i--) {
-            let sum = Math.min(height[left], height[i]) * (i - left)
-            max = Math.max(sum, max);
-        }
-        result = Math.max(result, max);
-        left += 1;
+        const sum = Math.min(height[left], height[right]) * (right - left);
+        result = Math.max(sum, result);
+        if(height[left] > height[right]) right -= 1;
+        else left += 1;
     }
 
     return result;
