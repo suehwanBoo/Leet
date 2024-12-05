d6ed364981df52fcf5841f8e652b7c1b051b4081
suehwanBoo <130543156+suehwanBoo@users.noreply.github.com>
2024-12-05 13:36:34 +0900

    Time: 9 ms (10.47%), Space: 52.7 MB (8.48%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.js
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.js
@@ -3,13 +3,13 @@
  * @return {number}
  */
 var maxProduct = function(nums) {
+    const dp = Array.from({length : nums.length + 1}, () => [1,1]);
     let result = -Infinity;
-    for(let i = 0; i < nums.length; i++) {
-        let current = 1;
-        for(let j = i; j < nums.length; j++) {
-            current *= nums[j];
-            if(result < current) result = current;
-        }
+    for(let i = 1; i < dp.length; i++) {
+        const num = nums[i - 1];
+        dp[i][0] = Math.max(dp[i - 1][0] * num, dp[i - 1][1] * num, num);
+        dp[i][1] = Math.min(dp[i - 1][0] * num, dp[i - 1][1] * num, num);
+        result = Math.max(result, dp[i][0])
     }
     return result;
 };
