8947d23ab68f889a8a6a042ee358f642dc6c23d3
suehwanBoo <130543156+suehwanBoo@users.noreply.github.com>
2024-12-08 15:54:49 +0900

    Time: 45 ms (5.07%), Space: 60.5 MB (7.45%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,13 +3,13 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    const dp = Array.from({length: prices.length + 1}, () => -Infinity);
-    let minBuy = Infinity;
+    const dp = Array.from({length: prices.length + 1}, () => 0);
+    let minBuy = prices[0];
     for(let i = 1; i <= prices.length; i++) {
         dp[i] = Math.max(dp[i-1], prices[i - 1] - minBuy);
         minBuy = Math.min(prices[i - 1], minBuy)
     }
 
-    return dp[prices.length] > 0 ? dp[prices.length] : 0;
+    return dp[prices.length];
     
 };
