697136ba4ef44fce0195fa20b64a37668ec25b87
suehwanBoo <130543156+suehwanBoo@users.noreply.github.com>
2024-12-09 19:51:51 +0900

    Time: 22 ms (85.1%), Space: 60 MB (59.65%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.js
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.js
@@ -3,33 +3,34 @@
  * @return {number[][]}
  */
 var pacificAtlantic = function(heights) {
-    function isBoth(i,j) {
-        const save = [false,false];
-        const DIRECT = [[0,1], [1,0], [-1,0], [0,-1]];
-        const M = heights.length;
-        const N = heights[0].length;
-        const visit = {[`${i}-${j}`] : true}
-        function dfs(i,j) {
-            if(i === 0 || j === 0) save[0] = true;
-            if(i === M - 1 || j === N - 1) save[1] = true;
-            if(save[0] === true && save[1] === true) return true;
-            for(let [ai, aj] of DIRECT) {
-                const ni = ai + i;
-                const nj = aj + j;
-                if(0 <= ni && ni < M && 0<= nj && nj < N && !visit[`${ni}-${nj}`] && heights[i][j] >= heights[ni][nj]) {
-                    visit[`${ni}-${nj}`] = true;
-                    dfs(ni,nj);
+    const result = [], M = heights.length, N = heights[0].length;
+    const dp = Array.from({length : M}, () => Array.from({length : N}).fill(0));
+    const DIRECT = [[0,1],[1,0],[-1,0],[0,-1]];
+    function mark(i,j, color) {
+        if(dp[i][j] === 0) dp[i][j] = color;
+        else if(dp[i][j] !== color && dp[i][j] !== 3) {
+            result.push([i,j]);
+            dp[i][j] = 3;
+        }
+        for(let [ai,aj] of DIRECT) {
+            const ni = ai + i;
+            const nj = aj + j;
+            if(0<= ni && ni < M && 0<= nj && nj < N && heights[i][j] <= heights[ni][nj]) {
+                if(dp[ni][nj] !== 3 && dp[ni][nj] !== color) {
+                    mark(ni,nj,color);
                 }
             }
         }
-        dfs(i,j);
-        return save[0] === true && save[1] === true;
+
     }
-    const result = [];
-    for(let i = 0; i < heights.length; i++) {
-        for(let j = 0; j< heights[i].length; j++) {
-            if(isBoth(i,j)) result.push([i,j]);
-        }
+
+    for(let i = 0; i < M; i++) {
+        mark(i, 0, 1);
+        mark(i, N - 1, 2);
+    }
+    for(let j = 0; j < N; j++) {
+        mark(0, j, 1);
+        mark(M -1 , j, 2);
     }
     return result;
 };
