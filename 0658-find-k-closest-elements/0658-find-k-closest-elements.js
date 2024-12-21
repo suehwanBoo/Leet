3251baf2e1122cbae58c01332b6d25634812af1c
suehwanBoo <130543156+suehwanBoo@users.noreply.github.com>
2024-12-21 19:19:11 +0900

    Time: 39 ms (9.22%), Space: 59.1 MB (8.68%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.js
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.js
@@ -15,33 +15,28 @@ class HeapQ {
 
     push(elem) {
         const store = this.store;
-        if(store.length < this.limit) {
+        if (store.length < this.limit) {
             store.push(elem);
             this._bubbleUp();
-            return;
         } else {
             const elemPoint = Math.abs(elem - this.closet);
-            const headPoint = this.getPoint(0)
-            if(elemPoint < headPoint || (elemPoint === headPoint && elem < store[0])) {
+            const headPoint = this.getPoint(0);
+            if (elemPoint < headPoint || (elemPoint === headPoint && elem < store[0])) {
                 store[0] = elem;
                 this._bubbleDown();
             }
         }
     }
+
     _bubbleUp() {
         const store = this.store;
-        const closet = this.closet;
         let pointer = store.length - 1;
-        let parent = this._getParent(pointer);
-        while(pointer > parent && parent >= 0) {
-            const nowPoint = this.getPoint(pointer);
-            const parentPoint = this.getPoint(parent);
-            if(nowPoint > parentPoint || (nowPoint === parentPoint && store[pointer] > store[parent])) {
+        while (pointer > 0) {
+            const parent = this._getParent(pointer);
+            if (this._compare(pointer, parent)) {
                 [store[pointer], store[parent]] = [store[parent], store[pointer]];
                 pointer = parent;
-                parent = this._getParent(parent);
-            }
-            else break;
+            } else break;
         }
     }
 
@@ -49,14 +44,12 @@ class HeapQ {
         const store = this.store;
         let pointer = 0;
         let child = this._getChild(pointer);
-        while(child) {
-            const nowPoint = this.getPoint(pointer);
-            const childPoint = this.getPoint(child);
-            if(nowPoint < childPoint || (nowPoint === childPoint && store[pointer] < store[child])) {
+        while (child !== null) {
+            if (this._compare(child, pointer)) {
                 [store[pointer], store[child]] = [store[child], store[pointer]];
                 pointer = child;
-                child = this._getChild(child);
-            }
+                child = this._getChild(pointer);
+            } else break;
         }
     }
 
@@ -64,20 +57,27 @@ class HeapQ {
         const store = this.store;
         const left = idx * 2 + 1;
         const right = idx * 2 + 2;
-        if(store.length <= left && store.length <= right) return null;
-        else if (store.length <= right) return left;
-        else return this.getPoint(left) > this.getPoint(right) || (this.getPoint(left) === this.getPoint(right) && store[left] > store[right]) ? left : right;
+        if (left >= store.length) return null;
+        if (right >= store.length) return left;
+        return this._compare(left, right) ? left : right;
     }
-    
+
     _getParent(idx) {
-        return Math.floor((idx - 1) / 2)
+        return Math.floor((idx - 1) / 2);
+    }
+
+    _compare(idx1, idx2) {
+        const store = this.store;
+        const point1 = this.getPoint(idx1);
+        const point2 = this.getPoint(idx2);
+        return point1 > point2 || (point1 === point2 && store[idx1] > store[idx2]);
     }
 
     getPoint(idx) {
         return Math.abs(this.store[idx] - this.closet);
     }
-
 }
+
 
 
 var findClosestElements = function(arr, k, x) {
